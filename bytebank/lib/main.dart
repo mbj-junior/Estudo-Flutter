373ae3d47700eb28753768cd26ccaf7f8aa0ce4a
Marcio Botelho Jr <mbj_junior@hotmail.com>
2020-07-06 23:06:30 -0300

    Criando Widget

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -1,3 +1,26 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Text('Hello World!', textDirection: TextDirection.ltr,));
+void main() => runApp(
+      Column(
+        children: <Widget>[
+          Text(
+            'Deliver features faster',
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'Craft beautiful UIs',
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'Craft beautiful UIs',
+            textDirection: TextDirection.ltr,
+          ),
+          Column(children: <Widget>[
+            Text(
+              'Craft beautiful UIs',
+              textDirection: TextDirection.ltr,
+            ),
+          ])
+        ], //<Widget>[]
+      ), //Column
+    );
