14c7ec8b024dce03d8b396fef9204ca3a00a84e3
marciob <marciob@cpqd.com.br>
2021-04-07 23:35:05 -0300

    LIsta por Json

--- cozinhando_casa/lib/main.dart
+++ cozinhando_casa/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './screen/home/home.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,25 +9,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.deepOrange,
-      ),
-      home: Scaffold(
-        body: SizedBox(
-          height: 300,
-          child: Card(
-            child: Column(
-              children: <Widget>[
-                Image.network(
-                    'https://amp.receitadevovo.com.br/wp-content/uploads/2020/10/receita-de-calda-de-chocolate-para-bolo.jpg'),
-                Text('Bolo de Chocolate')
-              ],
-            ),
-          ),
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.deepOrange,
         ),
-        appBar: AppBar(title: Text('Cozinando em Casa')),
-      ),
-    );
+        home: Home());
   }
 }
+
+
---
+++ cozinhando_casa/lib/screen/home/home.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import '../../model/receita.dart';
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new HomeState();
+  }
+}
+
+class HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return _construirHome();
+  }
+
+  Widget _construirHome() {
+    return Scaffold(
+      body: _construirListaCard(),
+      appBar: _construirAppBar(),
+    );
+  }
+
+  Widget _construirListaCard() {
+    return FutureBuilder(
+      future: DefaultAssetBundle.of(context).loadString('assets/receitas.json'),
+      builder: (context, snapshot) {
+        List<dynamic> receitas = json.decode(
+          snapshot.data.toString(),
+        );
+        return ListView.builder(itemBuilder: (BuildContext context, int index){
+          Receita receita = Receita.fromJson(receitas[index]);
+          return _construirCard(receita.titulo, receita.foto);
+        },itemCount: receitas == null ? 0 : receitas.length,);
+      },
+    );
+  }
+
+  Widget _construirCard(titulo, foto) {
+    return SizedBox(
+      height: 300,
+      child: Card(
+        margin: EdgeInsets.all(16),
+        child: Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                _construirImagens(foto),
+                _construirTextoCard(titulo),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _construirTextoCard(titulo) {
+    return Positioned(
+      bottom: 10,
+      left: 10,
+      child: Text(
+        titulo,
+        style: TextStyle(fontSize: 20),
+      ),
+    );
+  }
+
+  Widget _construirImagens(foto) {
+    return Image.asset(
+      foto,
+      fit: BoxFit.fill,
+      height: 268,
+    );
+  }
+
+  Widget _construirAppBar() {
+    return AppBar(title: Text('Cozinando em Casa'));
+  }
+}
