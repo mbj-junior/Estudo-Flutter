3c8a24d42ed7b92ff699bec92877c708fdfea6b7
marciob <marciob@cpqd.com.br>
2021-09-01 23:48:24 -0300

    Apresenta na tela os valores da transaction

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -29,25 +29,30 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Despesas Pessoais'),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text('Grafico'),
-                elevation: 5,
-              ),
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('Grafico'),
+              elevation: 5,
             ),
-            Card(
-              child: Text('Lista de transações'),
-            )
-          ],
-        ));
+          ),
+          Column(children: <Widget>[
+            ..._transactions.map((tr) {
+              return Card(
+                child: Text(tr.title),
+              );
+            }).toList(),
+          ])
+        ],
+      ),
+    );
   }
 }
