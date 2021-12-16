4a39a21ec88baf438deedfe1ac318edff76a3aea
Márcio Botelho Junior <mbj_junior@hotmail.com>
2021-12-16 00:33:21 -0300

    introdução variaveis dart

---
+++ Dart/variaveis/bin/variaveis.dart
@@ -0,0 +1,15 @@
+void main() {
+  // 1 - Introdução a Variaveis
+
+  String variavelNome = "Marcio";
+  print(variavelNome);
+
+  int valorVariavel = 10;
+  print(valorVariavel);
+
+  bool ehVerdadeiro = true;
+  print(ehVerdadeiro);
+  
+  List<String> listaDePalavras = ["Marcio", "Botelho", "Junior"];
+  print('${listaDePalavras[0]} ${listaDePalavras[1]} ${listaDePalavras[2]}');
+}
