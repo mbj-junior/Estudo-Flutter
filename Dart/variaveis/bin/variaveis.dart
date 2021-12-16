447131316cb56d1c71b5e453674b00f032daae93
Márcio Botelho Junior <mbj_junior@hotmail.com>
2021-12-16 11:42:55 -0300

    anotações

--- Dart/variaveis/bin/variaveis.dart
+++ Dart/variaveis/bin/variaveis.dart
@@ -9,7 +9,21 @@ void main() {
 
   bool ehVerdadeiro = true;
   print(ehVerdadeiro);
-  
+
   List<String> listaDePalavras = ["Marcio", "Botelho", "Junior"];
   print('${listaDePalavras[0]} ${listaDePalavras[1]} ${listaDePalavras[2]}');
+
+  // 2 - introdução null-safety
+
+  //com ? a variável pode aceitar null
+  String? nome;
+  nome = "ABC";
+  nome = null;
+  // com ! não aceita que seja null
+  print(nome!);
+
+  // late aceita null na criação
+  late String sobrenome;
+  sobrenome = "não é null";
+  print(sobrenome);
 }
