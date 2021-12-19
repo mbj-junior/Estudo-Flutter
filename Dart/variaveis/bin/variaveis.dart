7dfa4503b1f31fc843fcdae7f75fce426fdc3b90
Márcio Botelho Junior <mbj_junior@hotmail.com>
2021-12-19 15:56:26 -0300

    Orientação a objeto

--- Dart/variaveis/bin/variaveis.dart
+++ Dart/variaveis/bin/variaveis.dart
@@ -61,4 +61,50 @@ void main() {
     contador--;
     print(contador);
   }
+
+  // 5 - Métodos e classes
+  Celular celularDoMarcio = Celular("Azul", 5, 0.8, 5.7);
+  Celular celularDaAdila = Celular("Preto", 10, 0.100, 3.7);
+
+  print(celularDoMarcio.toString());
+  print(celularDaAdila.toString());
+  double resultado = celularDoMarcio.valorDoCelular(50);
+  print(resultado);
+
+  // 6 - Ortientação a objeto
+  Carro mercedes = Carro("Mercedes");
+  Carro gol = Carro("Gol");
+
+  print(mercedes.valorDoCarro);
+}
+
+// 5 - Métodos e classes
+class Celular {
+  final String cor;
+  final int qtdPros;
+  final double tamanho;
+  final double peso;
+
+  Celular(this.cor, this.qtdPros, this.peso, this.tamanho);
+
+  String toString() {
+    return "Cor $cor, qtdPros $qtdPros, tamanho $tamanho, peso $peso";
+  }
+
+  double valorDoCelular(double valor) {
+    return valor * qtdPros;
+  }
+}
+
+// 6 - Ortientação a objeto
+class Carro {
+  final String modelo;
+  String _segredo = "Muito dinheiro";
+  int _valor = 1000;
+
+  int get valorDoCarro => _valor;
+
+  void setValue(int valor) => _valor = valor;
+
+  Carro(this.modelo);
 }
