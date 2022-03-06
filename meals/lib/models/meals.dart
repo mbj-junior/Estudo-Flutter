83bee7d5deb8814a8cd064a4e2d55299c0300d6c
Márcio Botelho Junior <mbj_junior@hotmail.com>
2022-03-06 20:14:31 -0300

    feat: change modal meals

--- meals/lib/models/meals.dart
+++ meals/lib/models/meals.dart
@@ -31,4 +31,30 @@ class Meal {
     required this.isVegetarian,
     required this.isLactoseFree,
   });
+
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.simple:
+        return 'Simples';
+      case Complexity.medium:
+        return 'Normal';
+      case Complexity.difficult:
+        return 'Difícil';
+      default:
+        return 'Desconhecida';
+    }
+  }
+
+  String get costText {
+    switch (cost) {
+      case Cost.cheap:
+        return 'Barato';
+      case Cost.fair:
+        return 'Justo';
+      case Cost.expensive:
+        return 'Caro';
+      default:
+        return 'Desconhecido';
+    }
+  }
 }
