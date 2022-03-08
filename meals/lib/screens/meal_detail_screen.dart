406abfe00159da6313ea2144b0897149dc3a076e
Márcio Botelho Junior <mbj_junior@hotmail.com>
2022-03-07 23:35:36 -0300

    feat: create meals details

--- meals/lib/screens/meal_detail_screen.dart
+++ meals/lib/screens/meal_detail_screen.dart
@@ -8,10 +8,46 @@ class MealDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final meal = ModalRoute.of(context)?.settings.arguments as Meal;
     return Scaffold(
-      appBar: AppBar(title: Text(meal.title)),
-      body: const Center(
-        child: Text('Detalhe da refeição'),
-      ),
-    );
+        appBar: AppBar(title: Text(meal.title)),
+        body: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                meal.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 10),
+              child: Text(
+                'Ingredientes',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            ),
+            Container(
+                width: 300,
+                height: 200,
+                padding: const EdgeInsets.all(10),
+                margin: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(10)),
+                child: ListView.builder(
+                    itemCount: meal.ingredients.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 5, horizontal: 10),
+                          child: Text(meal.ingredients[index]),
+                        ),
+                        color: Theme.of(context).colorScheme.secondary,
+                      );
+                    })),
+          ],
+        ));
   }
 }
