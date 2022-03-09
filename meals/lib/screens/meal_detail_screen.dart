9ef6be0b4a7e8493cc8a3933dd209a116e769612
Márcio Botelho Junior <mbj_junior@hotmail.com>
2022-03-08 21:45:35 -0300

    feat: create meal detail screen

--- meals/lib/screens/meal_detail_screen.dart
+++ meals/lib/screens/meal_detail_screen.dart
@@ -9,45 +9,76 @@ class MealDetailScreen extends StatelessWidget {
     final meal = ModalRoute.of(context)?.settings.arguments as Meal;
     return Scaffold(
         appBar: AppBar(title: Text(meal.title)),
-        body: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 300,
-              width: double.infinity,
-              child: Image.network(
-                meal.imageUrl,
-                fit: BoxFit.cover,
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 300,
+                width: double.infinity,
+                child: Image.network(
+                  meal.imageUrl,
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 10),
-              child: Text(
-                'Ingredientes',
-                style: Theme.of(context).textTheme.titleMedium,
-              ),
-            ),
-            Container(
-                width: 300,
-                height: 200,
-                padding: const EdgeInsets.all(10),
-                margin: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(color: Colors.grey),
-                    borderRadius: BorderRadius.circular(10)),
-                child: ListView.builder(
-                    itemCount: meal.ingredients.length,
+              _createSectionTitle(context, 'Ingredientes'),
+              _createSectionContainer(ListView.builder(
+                  itemCount: meal.ingredients.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 5, horizontal: 10),
+                        child: Text(meal.ingredients[index]),
+                      ),
+                      color: Theme.of(context).colorScheme.secondary,
+                    );
+                  })),
+              _createSectionTitle(context, 'Passos'),
+              _createSectionContainer(
+                ListView.builder(
+                    itemCount: meal.steps.length,
                     itemBuilder: (context, index) {
-                      return Card(
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 5, horizontal: 10),
-                          child: Text(meal.ingredients[index]),
-                        ),
-                        color: Theme.of(context).colorScheme.secondary,
+                      return Column(
+                        children: [
+                          ListTile(
+                            leading: CircleAvatar(
+                              child: Text('${index + 1}'),
+                            ),
+                            title: Text(meal.steps[index]),
+                          ),
+                          Divider(
+                            color: Colors.grey.shade600,
+                            thickness: 1.5,
+                          )
+                        ],
                       );
-                    })),
-          ],
+                    }),
+              ),
+            ],
+          ),
         ));
+  }
+
+  Widget _createSectionTitle(BuildContext context, String title) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.titleMedium,
+      ),
+    );
+  }
+
+  Widget _createSectionContainer(Widget child) {
+    return Container(
+        width: 330,
+        height: 200,
+        padding: const EdgeInsets.all(10),
+        margin: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(10)),
+        child: child);
   }
 }
