935434ca89e4bf30674a6902b31d6d0984d85090
Márcio Botelho Junior <mbj_junior@hotmail.com>
2022-03-09 21:36:12 -0300

    feat: create the botton tabs Screen

--- meals/lib/main.dart
+++ meals/lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:meals/utils/app_routes.dart';
-import 'package:meals/screens/categories_screen.dart';
+import 'package:meals/screens/tabs_screen.dart';
 import 'package:meals/screens/meal_detail_screen.dart';
 import 'package:meals/screens/categories_meals_screen.dart';
-import 'package:meals/screens/tabs_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -16,9 +15,8 @@ class MyApp extends StatelessWidget {
       title: 'DeliMeals',
       theme: ThemeData(
           fontFamily: 'Raleway',
+          primarySwatch: Colors.pink,
           canvasColor: const Color.fromRGBO(255, 254, 229, 1),
-          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink)
-              .copyWith(secondary: Colors.amber),
           textTheme: ThemeData.light().textTheme.copyWith(
                 titleMedium: const TextStyle(
                   fontSize: 18,
--- meals/lib/screens/tabs_screen.dart
+++ meals/lib/screens/tabs_screen.dart
@@ -2,34 +2,52 @@ import 'package:flutter/material.dart';
 import 'package:meals/screens/favorite_screen.dart';
 import 'package:meals/screens/categories_screen.dart';
 
-class TabsScreen extends StatelessWidget {
+class TabsScreen extends StatefulWidget {
   const TabsScreen({Key? key}) : super(key: key);
 
   @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedScreenIndex = 0;
+
+  final List<Map<String, dynamic>> _screens = [
+    {'title': 'Lista de Categorias', 'screen': const CategoriesScreen()},
+    {'title': 'Favoritos', 'screen': const FavoriteScreen()}
+  ];
+
+  _selectedScreen(int index) {
+    setState(() {
+      _selectedScreenIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Vamos cozinhar'),
-          bottom: const TabBar(tabs: [
-            Tab(
-              icon: Icon(Icons.category),
-              text: 'Categoria',
-            ),
-            Tab(
-              icon: Icon(Icons.star),
-              text: 'Favoritos',
-            )
-          ]),
-        ),
-        body: const TabBarView(
-          children: [
-            CategoriesScreen(),
-            FavoriteScreen(),
-          ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          _screens[_selectedScreenIndex]['title'],
         ),
       ),
+      body: _screens[_selectedScreenIndex]['screen'],
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: _selectedScreen,
+          unselectedItemColor: Colors.white,
+          selectedItemColor: Colors.amber,
+          currentIndex: _selectedScreenIndex,
+          type: BottomNavigationBarType.shifting,
+          items: [
+            BottomNavigationBarItem(
+                backgroundColor: Theme.of(context).primaryColor,
+                icon: const Icon(Icons.category),
+                label: 'Categorias'),
+            BottomNavigationBarItem(
+                backgroundColor: Theme.of(context).primaryColor,
+                icon: const Icon(Icons.star),
+                label: 'Favoritos'),
+          ]),
     );
   }
 }
