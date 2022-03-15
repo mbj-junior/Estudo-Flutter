ca23cf55111ffb04c31fc57198f3e4352f33df69
Márcio Botelho Junior <mbj_junior@hotmail.com>
2022-03-15 19:45:33 -0300

    feat: create settings

---
+++ meals/lib/models/settings.dart
@@ -0,0 +1,13 @@
+class Settings {
+  late bool isGluttenFree;
+  late bool isVegan;
+  late bool isVegetarian;
+  late bool isLactoseFree;
+
+  Settings({
+    this.isGluttenFree = false,
+    this.isVegan = false,
+    this.isVegetarian = false,
+    this.isLactoseFree = false,
+  });
+}
--- meals/lib/screens/settings_screen.dart
+++ meals/lib/screens/settings_screen.dart
@@ -1,19 +1,80 @@
 import 'package:flutter/material.dart';
 import 'package:meals/components/main_drawer.dart';
+import 'package:meals/models/settings.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  var settings = Settings();
+
+  Widget _createSwitch(
+    String title,
+    String subtitle,
+    bool value,
+    void Function(bool)? onChanged,
+  ) {
+    return SwitchListTile.adaptive(
+        title: Text(title),
+        subtitle: Text(subtitle),
+        value: value,
+        onChanged: onChanged);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Configurações'),
-      ),
-      drawer: const MainDrawer(),
-      body: const Center(
-        child: Text('Configurações'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Configurações'),
+        ),
+        drawer: const MainDrawer(),
+        body: Column(
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: Text(
+                'Configurações',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            ),
+            Expanded(
+                child: ListView(
+              children: <Widget>[
+                _createSwitch(
+                    'Sem Glutem',
+                    'Só exibe refeições sem glutem',
+                    settings.isGluttenFree,
+                    (value) => setState(() {
+                          settings.isGluttenFree = value;
+                        })),
+                _createSwitch(
+                    'Sem Lactose',
+                    'Só exibe refeições sem lactose',
+                    settings.isLactoseFree,
+                    (value) => setState(() {
+                          settings.isLactoseFree = value;
+                        })),
+                _createSwitch(
+                    'Vegana',
+                    'Só exibe refeições Veganas',
+                    settings.isVegan,
+                    (value) => setState(() {
+                          settings.isVegan = value;
+                        })),
+                _createSwitch(
+                    'Vegetariana',
+                    'Só exibe refeições sem lactose',
+                    settings.isVegetarian,
+                    (value) => setState(() {
+                          settings.isVegetarian = value;
+                        })),
+              ],
+            ))
+          ],
+        ));
   }
 }
