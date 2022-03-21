81cfbf0e0f376b0034d5542aa72cc762930725d0
Márcio Botelho Junior <marcio.botelhojunior@gmail.com>
2022-03-21 14:46:23 -0300

    feate: config auth and analytics

---
+++ aws_awesome/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "awsawesome9a25be2e": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "analytics": {
+        "awsawesome": {
+            "Region": "string",
+            "Id": "string",
+            "appName": "string"
+        }
+    }
+}
