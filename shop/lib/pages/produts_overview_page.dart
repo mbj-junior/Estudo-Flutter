3327026e3c3610007caf02623dad05d44836394d
Márcio Botelho Junior <marcio.botelhojunior@gmail.com>
2022-03-31 11:02:28 -0300

    feat: create popMenu

--- shop/lib/pages/produts_overview_page.dart
+++ shop/lib/pages/produts_overview_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shop/components/product_grid.dart';
 
+enum FilterOptions { favorite, all }
 
 class ProductOverviewPage extends StatelessWidget {
   const ProductOverviewPage({Key? key}) : super(key: key);
@@ -10,10 +11,26 @@ class ProductOverviewPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha Loja'),
+        actions: [
+          PopupMenuButton(
+            icon: const Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                child: Text('Somente Favoritos'),
+                value: FilterOptions.favorite,
+              ),
+              const PopupMenuItem(
+                child: Text('Todos'),
+                value: FilterOptions.all,
+              ),
+            ],
+            onSelected: (FilterOptions selectValkue) {
+              debugPrint(selectValkue.name.toString());
+            },
+          )
+        ],
       ),
       body: const ProductGrid(),
     );
   }
 }
-
-
