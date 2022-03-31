cf58dba2e12c7f983af261f4067b77fc344ea70e
Márcio Botelho Junior <marcio.botelhojunior@gmail.com>
2022-03-31 15:09:12 -0300

    feat: intl

--- poc_intl/lib/main.dart
+++ poc_intl/lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    localizationsDelegates: AppLocalizations.localizationsDelegates,
+    supportedLocales: AppLocalizations.supportedLocales,
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -12,14 +18,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
-          title: const Center(
-        child: Text('Aplicativo em Porugues'),
+          title: Center(
+        child: Text(AppLocalizations.of(context)!.title),
       )),
-      body: const Center(
+      body: Center(
         child: Text(
-          'Olá Mundo',
-          style: TextStyle(fontSize: 50, color: Colors.black),
+          AppLocalizations.of(context)!.helloWorld,
+          style: const TextStyle(fontSize: 50, color: Colors.black),
         ),
+        
       ),
     ));
   }
