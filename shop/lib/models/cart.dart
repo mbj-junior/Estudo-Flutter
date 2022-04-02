020c97c8f5b1ea6b806028f8cd31d306ffa0a573
Márcio Botelho Junior <marcio.botelhojunior@gmail.com>
2022-04-02 19:55:11 -0300

    feat: create cart and cart_items

--- shop/lib/models/cart.dart
+++ shop/lib/models/cart.dart
@@ -5,12 +5,21 @@ import 'package:shop/models/cart_item.dart';
 
 class Cart extends ChangeNotifier {
   Map<String, CartItem> _items = {};
+
   Map<String, CartItem> get items {
     return {..._items};
   }
 
   int get itemsCount {
     return _items.length;
+  }
+
+  double get totalAmount {
+    double total = 0.0;
+    _items.forEach((key, cartItem) {
+      total += cartItem.price * cartItem.quantity;
+    });
+    return total;
   }
 
   void addItem(Product product) {
