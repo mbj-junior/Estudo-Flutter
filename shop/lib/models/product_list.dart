350822fe072d03daf53955cfc2e37a1f4d5ec035
Márcio Botelho Junior <marcio.botelhojunior@gmail.com>
2022-10-09 15:58:58 -0300

    feat: create the method to patch

--- shop/lib/models/product_list.dart
+++ shop/lib/models/product_list.dart
@@ -5,7 +5,7 @@ import 'package:shop/models/product.dart';
 import 'package:http/http.dart' as http;
 
 class ProductList with ChangeNotifier {
-  final _url = 'https://shop-cod4r-default-rtdb.firebaseio.com/products.json';
+  final _baseUrl = 'https://shop-cod4r-default-rtdb.firebaseio.com/products';
   final List<Product> _items = [];
   List<Product> get items => [..._items];
 
@@ -18,7 +18,9 @@ class ProductList with ChangeNotifier {
 
   Future<void> loadProducts() async {
     _items.clear();
-    final response = await http.get(Uri.parse(_url));
+    final response = await http.get(
+      Uri.parse('$_baseUrl.json'),
+    );
     if (response.body == 'null') return;
     Map<String, dynamic> data = jsonDecode(response.body);
     data.forEach((productId, productData) {
@@ -56,7 +58,7 @@ class ProductList with ChangeNotifier {
 
   Future<void> addProduct(Product product) async {
     final response = await http.post(
-      Uri.parse(_url),
+      Uri.parse('$_baseUrl.json'),
       body: jsonEncode(
         {
           "name": product.name,
@@ -67,6 +69,7 @@ class ProductList with ChangeNotifier {
         },
       ),
     );
+
     final id = jsonDecode(response.body)['name'];
     _items.add(
       Product(
@@ -81,10 +84,22 @@ class ProductList with ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> updateProduct(Product product) {
+  Future<void> updateProduct(Product product) async {
     int index = _items.indexWhere((element) => element.id == product.id);
 
     if (index >= 0) {
+      await http.patch(
+        Uri.parse('$_baseUrl/${product.id}.json'),
+        body: jsonEncode(
+          {
+            "name": product.name,
+            "description": product.description,
+            "price": product.price,
+            "imageUrl": product.imageUrl,
+          },
+        ),
+      );
+
       _items[index] = product;
       notifyListeners();
     }
