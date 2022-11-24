5437b9d36871f04e006db27a1ea14d71abc5b2b1
Márcio Botelho Junior <marcio.botelhojunior@gmail.com>
2022-11-23 23:23:32 -0300

    chore add load in button buy

--- shop/lib/pages/cart_page.dart
+++ shop/lib/pages/cart_page.dart
@@ -70,25 +70,31 @@ class CartButton extends StatefulWidget {
 }
 
 class _CartButtonState extends State<CartButton> {
+  bool _isLoading = false;
+
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: widget.cart.itemsCount == 0
-          ? null
-          : () {
-              Provider.of<OrderList>(
-                context,
-                listen: false,
-              ).addOrder(widget.cart);
+    return _isLoading
+        ? const CircularProgressIndicator()
+        : TextButton(
+            onPressed: widget.cart.itemsCount == 0
+                ? null
+                : () async {
+                    setState(() => _isLoading = true);
+                    await Provider.of<OrderList>(
+                      context,
+                      listen: false,
+                    ).addOrder(widget.cart);
 
-              widget.cart.clear();
-            },
-      child: const Text("COMPRAR"),
-      style: TextButton.styleFrom(
-        textStyle: const TextStyle(
-          color: Colors.purple,
-        ),
-      ),
-    );
+                    widget.cart.clear();
+                    setState(() => _isLoading = false);
+                  },
+            child: const Text("COMPRAR"),
+            style: TextButton.styleFrom(
+              textStyle: const TextStyle(
+                color: Colors.purple,
+              ),
+            ),
+          );
   }
 }
