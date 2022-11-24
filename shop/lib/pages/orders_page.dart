95cf61b923662e004062e6e7ca2da847e00642f8
Márcio Botelho Junior <marcio.botelhojunior@gmail.com>
2022-11-24 00:20:40 -0300

    refactor: change setState to FutureBuilder

--- shop/lib/pages/orders_page.dart
+++ shop/lib/pages/orders_page.dart
@@ -4,43 +4,37 @@ import 'package:shop/components/order.dart';
 import 'package:shop/models/order_list.dart';
 import 'package:provider/provider.dart';
 
-class OrdersPage extends StatefulWidget {
+class OrdersPage extends StatelessWidget {
   const OrdersPage({Key? key}) : super(key: key);
 
   @override
-  State<OrdersPage> createState() => _OrdersPageState();
-}
-
-class _OrdersPageState extends State<OrdersPage> {
-  bool _isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    Provider.of<OrderList>(
-      context,
-      listen: false,
-    ).loadOrders().then((value) {
-      setState(() => _isLoading = false);
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final OrderList orders = Provider.of(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Meus Pedidos"),
       ),
       drawer: const AppDrawer(),
-      body: _isLoading
-          ? const Center(
+      body: FutureBuilder(
+        future: Provider.of<OrderList>(context, listen: false).loadOrders(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
               child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: orders.itemsCount,
-              itemBuilder: (context, index) =>
-                  OrderWidget(order: orders.items[index])),
+            );
+          } else if (snapshot.error != null) {
+            return const Center(child: Text("Error ao buscar os dados"));
+          } else {
+            return Consumer<OrderList>(
+              builder: (context, orders, child) => ListView.builder(
+                itemCount: orders.itemsCount,
+                itemBuilder: (context, index) => OrderWidget(
+                  order: orders.items[index],
+                ),
+              ),
+            );
+          }
+        },
+      ),
     );
   }
 }
