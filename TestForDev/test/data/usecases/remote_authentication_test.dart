e6fb6dbb6d8d93d0366b41989a9cd39324a238fb
Márcio Botelho Junior <marcio.botelhojunior@gmail.com>
2023-01-17 22:29:44 -0300

    ensure RemoteAuthentication throws UnexpectedError on 404

--- TestForDev/test/data/usecases/remote_authentication_test.dart
+++ TestForDev/test/data/usecases/remote_authentication_test.dart
@@ -29,7 +29,7 @@ void main() {
         body: {"email": params.email, "password": params.secret}));
   });
 
-  test("Should throw UnexpectedError if HttpCliente returns 400", () async {
+  test("Should throw UnexpectedError if HttpClient returns 400", () async {
     when(
       httpClient.request(
         url: anyNamed("url"),
@@ -37,6 +37,20 @@ void main() {
         body: anyNamed("body"),
       ),
     ).thenThrow(HttpError.badRequest);
+
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test("Should throw UnexpectedError if HttpClient returns 404", () async {
+    when(
+      httpClient.request(
+        url: anyNamed("url"),
+        method: anyNamed("method"),
+        body: anyNamed("body"),
+      ),
+    ).thenThrow(HttpError.notFound);
 
     final future = sut.auth(params);
 
