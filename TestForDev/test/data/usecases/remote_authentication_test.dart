db26ffbb831283e0e06c678d6d95b4376ab7543f
Márcio Botelho Junior <marcio.botelhojunior@gmail.com>
2023-01-31 22:17:51 -0300

    feat: ensure RemoteAuthentication throws UnexpectedError on 200 with invalid data

--- TestForDev/test/data/usecases/remote_authentication_test.dart
+++ TestForDev/test/data/usecases/remote_authentication_test.dart
@@ -96,6 +96,7 @@ void main() {
 
     expect(future, throwsA(DomainError.invalidCredentials));
   });
+
   test("Should throw InvalidCredentialsError if HttpClient returns 401",
       () async {
     final accessToken = faker.guid.guid();
@@ -115,5 +116,21 @@ void main() {
     final account = await sut.auth(params);
 
     expect(account.token, accessToken);
+  });
+
+  test(
+      "Should throw UnexpectedError if HttpClient returns 200 with invalid data",
+      () async {
+    when(
+      httpClient.request(
+        url: anyNamed("url"),
+        method: anyNamed("method"),
+        body: anyNamed("body"),
+      ),
+    ).thenAnswer((_) async => {"invalid_key": "invalid_value"});
+
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
   });
 }
