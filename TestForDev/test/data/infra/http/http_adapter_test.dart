816376d284345bba7da630d2a87258ba37ea4a6a
Márcio Botelho Junior <marcio.botelhojunior@gmail.com>
2023-02-03 23:56:19 -0300

    test: ensure HttpAdapter returns null if post returns 204 with data

--- TestForDev/test/data/infra/http/http_adapter_test.dart
+++ TestForDev/test/data/infra/http/http_adapter_test.dart
@@ -22,7 +22,11 @@ class HttpAdapter implements HttpClient {
     };
     final jsonBody = body != null ? jsonEncode(body) : null;
     final response = await client.post(url, headers: headers, body: jsonBody);
-    return response.body.isEmpty ? null : jsonDecode(response.body);
+    if (response.statusCode == 200) {
+      return response.body.isEmpty ? null : jsonDecode(response.body);
+    } else {
+      return null;
+    }
   }
 }
 
@@ -89,9 +93,17 @@ void main() {
 
       expect(response, null);
     });
-    
+
     test("Shoul return null if post returns 204", () async {
       mockResponse(204, body: '');
+
+      final response = await sut.request(url: url, method: "post");
+
+      expect(response, null);
+    });
+
+    test("Shoul return null if post returns 204 with data", () async {
+      mockResponse(204);
 
       final response = await sut.request(url: url, method: "post");
 
