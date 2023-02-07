3a508604c2b7b3328f813f95983f8628cfea4951
Márcio Botelho Junior <marcio.botelhojunior@gmail.com>
2023-02-06 22:24:01 -0300

    chore: ensure HttpAdapter throws BadRequestError on 400

--- TestForDev/test/data/infra/http/http_adapter_test.dart
+++ TestForDev/test/data/infra/http/http_adapter_test.dart
@@ -1,3 +1,4 @@
+import 'package:TestForDev/data/http/http.dart';
 import 'package:TestForDev/infra/http/http.dart';
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
@@ -82,6 +83,14 @@ void main() {
       final response = await sut.request(url: url, method: "post");
 
       expect(response, null);
+    });
+
+    test("Shoul return BadRequestError if post returns 400", () async {
+      mockResponse(400);
+
+      final future = sut.request(url: url, method: "post");
+
+      expect(future, throwsA(HttpError.badRequest));
     });
   });
 }
