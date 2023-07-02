7340f75127faa7935bf9219bd8006a5d4a1c0f0c
Márcio Botelho Junior <marcio.botelhojunior@gmail.com>
2023-07-02 15:55:17 -0300

    test: verify lists

--- compare_arrays/test/compare_arrays_test.dart
+++ compare_arrays/test/compare_arrays_test.dart
@@ -1,8 +1,21 @@
-// import 'package:compare_arrays/compare_arrays.dart';
-// import 'package:test/test.dart';
+import 'package:test/test.dart';
 
-// void main() {
-//   test('calculate', () {
-//     expect(calculate(), 42);
-//   });
-// }
+import '../bin/compare_arrays.dart';
+
+void main() {
+  test('Must return true to equal list sorted', () {
+    expect(compareListInt([1, 2, 3], [1, 2, 3]), true);
+  });
+
+  test('Must return true to equal list dont sort', () {
+    expect(compareListInt([1, 2, 3], [3, 2, 1]), true);
+  });
+
+  test('Must return false to same length', () {
+    expect(compareListInt([1, 2, 2], [1, 2, 3]), false);
+  });
+
+  test('Must return false to diferent length', () {
+    expect(compareListInt([1, 2, 3], [3, 2, 1, 4]), false);
+  });
+}
