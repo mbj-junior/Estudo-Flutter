49afee76de0f9f13a5e6170cdd3c22c973e44b84
Márcio Botelho Junior <marcio.botelhojunior@gmail.com>
2023-07-02 09:34:13 -0300

    fix crop message

--- crop_message/bin/crop_message.dart
+++ crop_message/bin/crop_message.dart
@@ -1,11 +1,27 @@
-void main(List<String> arguments) {
-  String testMessage = "Codility We test coders test";
-  int k = 14;
+import 'dart:io';
 
-  solution(testMessage, k);
+void main(List<String> arguments) async {
+  String testMessage = "Codility We teste coders";
+  int testeK = 14;
+  // String? messageIn;
+  // String? kIn;
+
+  // while (messageIn == null) {
+  //   print("Digite a mensagem: ");
+  //   messageIn = stdin.readLineSync();
+  // }
+
+  // while (kIn == null) {
+  //   print("Digite o ponto de corte: ");
+  //   kIn = stdin.readLineSync();
+  // }
+
+  print(solution(testMessage, testeK));
+  // print(solution(messageIn, int.parse(kIn)));
 }
 
 String solution(String message, int k) {
+  final stopWatch = Stopwatch()..start();
   List<String> messageCrop = message.split(" ");
   int count = 0;
 
@@ -14,9 +30,10 @@ String solution(String message, int k) {
   for (String element in messageCrop) {
     if (count <= k) {
       finalMessage.add(element);
-      count += finalMessage.join(" ").length;
+      count += finalMessage.join(" ").length + 1;
     }
   }
-
+  stopWatch.stop();
+  print(stopWatch.elapsedTicks);
   return finalMessage.join(" ");
 }
---
+++ crop_message/test/crop_message_test.dart
@@ -0,0 +1,8 @@
+// import 'package:crop_message/crop_message.dart';
+// import 'package:test/test.dart';
+
+// void main() {
+//   test('calculate', () {
+//     expect(calculate(), 42);
+//   });
+// }
