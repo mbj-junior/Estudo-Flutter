266463719bb842d1f67e00e65d7364f03ebfb562
Márcio Botelho Junior <marcio.botelhojunior@gmail.com>
2023-07-02 15:09:21 -0300

    chore: initial

---
+++ compare_arrays/bin/compare_arrays.dart
@@ -0,0 +1,8 @@
+import 'package:compare_arrays/compare_arrays.dart' as compare_arrays;
+
+// Escrever uma função que receberia como parâmetro dois arrays 
+// de números inteiros desordenados e deveria retornar se ambos tinham os
+// mesmos valores, independentemente da ordem.
+void main(List<String> arguments) {
+  
+}
---
+++ compare_arrays/test/compare_arrays_test.dart
@@ -0,0 +1,8 @@
+// import 'package:compare_arrays/compare_arrays.dart';
+// import 'package:test/test.dart';
+
+// void main() {
+//   test('calculate', () {
+//     expect(calculate(), 42);
+//   });
+// }
