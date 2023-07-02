5f5d829a063dc46a7890ad97e44490fabbe46d0d
Márcio Botelho Junior <marcio.botelhojunior@gmail.com>
2023-07-02 15:54:59 -0300

    feat: verify lists

--- compare_arrays/bin/compare_arrays.dart
+++ compare_arrays/bin/compare_arrays.dart
@@ -1,8 +1,23 @@
-import 'package:compare_arrays/compare_arrays.dart' as compare_arrays;
-
-// Escrever uma função que receberia como parâmetro dois arrays 
+// Escrever uma função que receberia como parâmetro dois arrays
 // de números inteiros desordenados e deveria retornar se ambos tinham os
 // mesmos valores, independentemente da ordem.
+
 void main(List<String> arguments) {
-  
+  print(compareListInt([1, 2, 3, 4], [3, 2, 1, 4, 6]));
+}
+
+bool compareListInt(List<int> firstList, List<int> secondList) {
+  int countFistList = 0;
+  int countSecondList = 0;
+  bool isEqual = false;
+
+  if (firstList.length == secondList.length) {
+    for (int i = 0; i < firstList.length; i++) {
+      countFistList += firstList[i];
+      countSecondList += secondList[i];
+    }
+    isEqual = countFistList == countSecondList;
+  }
+
+  return isEqual;
 }
