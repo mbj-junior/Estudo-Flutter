76f10eb0e0708a44e2722d553f6c2d9d0fad4f42
Márcio Botelho Junior <marcio.botelhojunior@gmail.com>
2023-07-02 22:33:27 -0300

    test: add tests

--- min_max_five_array/lib/min_max_five_array.dart
+++ min_max_five_array/lib/min_max_five_array.dart
@@ -1,12 +1,14 @@
-String miniMaxSum(List<int> numbres) {
+String miniMaxSum(List<int> arr) {
   List<int> response = [0, 0];
 
-  for (int i = 0; i < numbres.length; i++) {
+  arr.sort();
+
+  for (int i = 0; i < arr.length; i++) {
     if (i != 4) {
-      response[0] += numbres[i];
+      response[0] += arr[i];
     }
     if (i != 0) {
-      response[1] += numbres[i];
+      response[1] += arr[i];
     }
   }
 
